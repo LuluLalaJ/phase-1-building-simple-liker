3c4e7647abc3c94a68d264c8f2e1b1d7a7119caf
Chen Jiang <chen.jiang.1206@gmail.com>
2023-03-27 14:59:10 -0400

    done

--- main.js
+++ main.js
@@ -5,6 +5,69 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+//add event listener to the heart 
+
+const likes = document.querySelectorAll('.like-glyph')
+//is there a way to use get element by ID?
+//any other way to select the like?
+likes.forEach(like => {
+  like.addEventListener('click', updateLikeOnServer)
+})
+
+function updateLikeOnServer(event) {
+//the call back of the event listener is to send a request to the server
+  const heart = event.target; 
+  mimicServerCall('serverURL')
+  //if successful:  the heart button changes 
+  //is it possible to pass on a callback function? 
+
+    // .then (() => {
+    //   if (heart.textContent === EMPTY_HEART) {
+    //       heart.textContent = FULL_HEART
+    //       heart.classList.add('activated-heart')
+    //   } else {
+    //       heart.textContent = EMPTY_HEART
+    //       heart.classList.remove('activated-heart')
+    //   }
+    // })
+
+    .then(() => updateLikeOnDOM(heart))
+
+  //if unsuccessful: the error message appears 
+
+    // .catch(error => {
+    //   const errorStatus = document.getElementById('modal').classList;
+    //   errorStatus.remove('hidden'); 
+    //   setTimeout(() => errorStatus.add('hidden'), 3000)
+    // })
+
+    .catch((error) => showError(error))
+}
+
+function updateLikeOnDOM(heart) {
+  if (heart.textContent === EMPTY_HEART) {
+    heart.textContent = FULL_HEART
+    heart.classList.add('activated-heart')
+  } else {
+    heart.textContent = EMPTY_HEART
+    heart.classList.remove('activated-heart')
+  }
+}
+
+function showError(error){
+    const errorStatus = document.getElementById('modal').classList;
+    errorStatus.remove('hidden'); 
+    setTimeout(hideError, 3000)
+//should not call the function in setTimeout; 
+//otherwise it will be executed immediately 
+
+}
+
+
+function hideError(){
+  const errorStatus = document.getElementById('modal');
+  errorStatus.classList.add('hidden'); 
+}
 
 
 //------------------------------------------------------------------------------
